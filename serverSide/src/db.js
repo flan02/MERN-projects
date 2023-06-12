7669c44a79a74aaa29d524558bd2021cf1d14883
flan02 <chanivetdan@hotmail.com>
2023-06-12 17:06:09 -0300

    Configuramos la conexion a Atlas ya con dotenv. Listo p/ arrancar cualquier proyecto desde 0.

--- serverSide/src/db.js
+++ serverSide/src/db.js
@@ -1,9 +1,15 @@
+
 import mongoose from 'mongoose'
+import {} from 'dotenv/config'
+
+const USER = process.env.USER
+const PASSWORD = process.env.PASSWORD
+const CLUSTER = process.env.CLUSTER
 
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://flan02:n5UvmrGtU2IIUx6a@cluster-rack.3how1p4.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@${CLUSTER}.3how1p4.mongodb.net/?retryWrites=true&w=majority`)
         console.log('>>> DB is connected.');
     } catch (error) {
         console.log(error);
--- serverSide/src/index.js
+++ serverSide/src/index.js
@@ -1,6 +1,7 @@
 //TODO desde aqui arrancamos la aplicacion
 import app from "./app.js";
 import { connectDB } from "./db.js";
+
 connectDB()
 app.listen(3000)
 console.log('Server on PORT at http://localhost:3000', 3000);
