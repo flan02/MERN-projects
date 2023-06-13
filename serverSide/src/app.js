b145bb9786ff2b2bd42bf2166f8d28a031929cbe
flan02 <chanivetdan@hotmail.com>
2023-06-13 14:25:12 -0300

    Agregamos CORS p/ permitir que de comunique nuestro puerto 3000 por apps que provengan dsd otros puertos.

--- serverSide/src/app.js
+++ serverSide/src/app.js
@@ -4,12 +4,17 @@ import morgan from 'morgan'
 import authRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:5173' // o indicamos qe solo se comuniqe dominio
+})) // si no ponemos 'origin' permitimos qe todos los dominios se comuniqen en este servidor.
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
-app.use('/api', authRoutes) //diferenciamos asi las rutas de React
+app.use('/api', authRoutes) 
 app.use('/api', taskRoutes)
 
 export default app
