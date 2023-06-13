78a240b8f070c3d391baa1b2f5f64a13c6b8575d
flan02 <chanivetdan@hotmail.com>
2023-06-13 15:58:05 -0300

    Hicimos el login de usuario con store en mongodb atlas. Verificar que por CORS la peticion POST se almacena como una OPTIONS en el Network del Google Chrome, en firefox dicen qe ese problema nose presenta.

--- clientSide/src/api/auth.js
+++ clientSide/src/api/auth.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-
+const opts = {
+    headers: {'X-Requested-With': 'XMLHttpRequest'}
+}
 const API = 'http://localhost:3000/api'
 // 2do param. es el req.body
-export const registerRequest = (user) => axios.post(`${API}/register`, user)
+export const registerRequest = (user) => axios.post(`${API}/register`, user, opts)
--- clientSide/src/pages/RegisterPage.jsx
+++ clientSide/src/pages/RegisterPage.jsx
@@ -1,50 +1,58 @@
-import { useForm } from 'react-hook-form'
-import { registerRequest } from '../api/auth'
+import { useForm } from "react-hook-form";
+import { registerRequest } from "../api/auth";
+
 //! Para autenticarnos de verdad tenemos qe hacer una peticion al backend con axios, fetch, ajax.
 //TODO La mayoria de apps modernas estan usando Axios.
 // Axios utiliza fetch por debajo.
 
 const RegisterPage = () => {
-
-    const { register, handleSubmit } = useForm()
-
+  const { register, handleSubmit } = useForm();
+  const onSubmit = handleSubmit(async (values) => {
+    //console.log(values);
+    const res = await registerRequest(values);
+    console.log(res);
+  });
   return (
     <>
-        <h1>Register</h1>
-        <div >
-            <form  className="form" onSubmit={handleSubmit(async (values) => {
-                console.log(values)
-                const res = await registerRequest(values)
-                console.log(res)
-            })} >
-            <div>
-                <label htmlFor="user"></label>
-                    <input type="text" id="user"
-                        { ...register("username", { required: true })} 
-                        placeholder='username'
-                        />
-            </div>
-            <div>
-                <label htmlFor="em"></label>
-                    <input type="email" id="em"
-                    { ...register("email", { required: true })} 
-                        placeholder='email'
-                    />
-            </div>
-            <div>
-                <label htmlFor="pass"></label>
-                <input type="password" id="pass"
-                    { ...register("password", { required: true, minLength: 6 })} 
-                        placeholder='password'
-                    />
-            </div>
-            <div>
-                <button type="submit"> Register </button>
-            </div>
-            </form>
-        </div>
+      <h1>Register</h1>
+      <div>
+        <form className="form" onSubmit={onSubmit}>
+          <div>
+            <label htmlFor="user"></label>
+            <input
+              type="text"
+              id="user"
+              {...register("username", { required: true })}
+              placeholder="username"
+              autoComplete="on"
+            />
+          </div>
+          <div>
+            <label htmlFor="em"></label>
+            <input
+              type="email"
+              id="em"
+              {...register("email", { required: true })}
+              placeholder="email"
+              autoComplete="on"
+            />
+          </div>
+          <div>
+            <label htmlFor="pass"></label>
+            <input
+              type="password"
+              id="pass"
+              {...register("password", { required: true, minLength: 6 })}
+              placeholder="password"
+            />
+          </div>
+          <div>
+            <button type="submit"> Register </button>
+          </div>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
