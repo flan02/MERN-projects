21ea75ad9e123ebb7f8181a765cf8be477965fa0
flan02 <chanivetdan@hotmail.com>
2023-06-30 23:42:21 -0300

    Agregamos las funciones de update and delete, validamos con postman, validamos posibles errores.

--- codingInFlow-todo-app-typescript/backend/src/routes/notes.ts
+++ codingInFlow-todo-app-typescript/backend/src/routes/notes.ts
@@ -5,4 +5,6 @@ const router = express.Router()
 router.get("/", notesController.getNotes)
 router.get("/:noteId", notesController.getNote)
 router.post("/", notesController.createNote)
+router.patch("/:noteId", notesController.updateNote) // patch p/ actualizar
+router.delete("/:noteId", notesController.deleteNote) // patch p/ actualizar
 export default router
