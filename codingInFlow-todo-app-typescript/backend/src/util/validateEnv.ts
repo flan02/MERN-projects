82d930b592db4a18ed37126b83a279ad25da3dda
flan02 <chanivetdan@hotmail.com>
2023-07-01 00:02:23 -0300

    Refactorizamos los directorios del backend para sumar al proyecto el frontend en vite react.

--- codingInFlow-todo-app-typescript/backend/src/util/validateEnv.ts
+++
@@ -1,7 +0,0 @@
-import { cleanEnv } from "envalid"
-import { str, port } from "envalid/dist/validators"
-
-export default cleanEnv(process.env, {
-    MONGO_CONNECTION_STRING: str(),
-    PORT: port()
-})
